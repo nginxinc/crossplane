91d3501fa1201ea3e2ef55f892af074d09324313
Luca Comellini <luca.comellini@nginx.com>
2018-04-18 18:20:51 -0700

    Fix string handling when there are quotes

--- crossplane/lexer.py
+++ crossplane/lexer.py
@@ -28,6 +28,7 @@ def _lex_file_object(file_obj):
     token = ''  # the token buffer
     token_line = 0  # the line the token starts on
     next_token_is_directive = True
+    quote_inside_token = False
 
     it = itertools.chain.from_iterable(file_obj)
     it = _iterescape(it)  # treat escaped characters differently
@@ -73,14 +74,19 @@ def _lex_file_object(file_obj):
         # if a quote is found, add the whole string to the token buffer
         if char in ('"', "'"):
             if token:
-                yield (token, token_line)
-                token = ''
+                token += char
+                quote_inside_token = True
 
             quote = char
             char, line = next(it)
             while char != quote:
                 token += quote if char == '\\' + quote else char
                 char, line = next(it)
+
+            if quote_inside_token:
+                token += quote
+                quote_inside_token = False
+                continue
 
             yield (token, token_line)
             if next_token_is_directive and token in EXTERNAL_LEXERS:
