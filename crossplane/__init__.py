aada518d168b563af6ddc0ddfbe29fe2ac262018
Arie van Luttikhuizen <aluttik@gmail.com>
2018-06-28 16:12:17 -0700

    Increased version to 0.4.5

--- crossplane/__init__.py
+++ crossplane/__init__.py
@@ -10,7 +10,7 @@ __title__ = 'crossplane'
 __summary__ = 'Reliable and fast NGINX configuration file parser.'
 __url__ = 'https://github.com/nginxinc/crossplane'
 
-__version__ = '0.4.4'
+__version__ = '0.4.5'
 
 __author__ = 'Arie van Luttikhuizen'
 __email__ = 'aluttik@gmail.com'
